ae1b8a6cd5b1e3fca1c5496573fc7936fb27611e
Ramom Silva <monmedeiros10@hotmail.com>
2024-11-27 23:44:09 -0300

    Iniciando implementações da Home

--- abastece_facil_adm/src/App.js
+++ abastece_facil_adm/src/App.js
@@ -1,19 +1,15 @@
-import logo from './logo.svg';
-import logoEmpresa from './assets/Backgorund 1.png';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={`${process.env.PUBLIC_URL}/logoEmpresa.png`} className="App-logo" alt="logo" />
-        <br/><br/><br/><br/>
-        <p>
-          Teste teste
-        </p>
-        <img src={logoEmpresa} alt="logo"/>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
